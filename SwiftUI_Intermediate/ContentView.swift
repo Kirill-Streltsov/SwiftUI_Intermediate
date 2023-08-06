f71f09a3fa2212535022d23a6584b43f292e0c85
Kirill-Streltsov <krlsrlsv@gmail.com>
2023-08-06 23:47:26 +0200

    cool real world example for a LongPressGesture

--- SwiftUI_Intermediate/ContentView.swift
+++ SwiftUI_Intermediate/ContentView.swift
@@ -8,16 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var isComplete: Bool = false
+    @State var isSuccess: Bool = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Rectangle()
+                .fill(isSuccess ? .green : .blue)
+                .frame(maxWidth: isComplete ? .infinity : 0)
+                .frame(height: 55)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .background(.gray)
+            HStack {
+                Text("Click here")
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(.black)
+                    .cornerRadius(10)
+                    .onLongPressGesture(minimumDuration: 1.0, maximumDistance: 50) { isPressing in
+                            // start of press -> min duration
+                        if isPressing {
+                            withAnimation(.easeInOut(duration: 1.0)) {
+                                isComplete = true
+                            }
+                        } else {
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                                if !isSuccess {
+                                    withAnimation(.easeInOut) {
+                                        isComplete = false
+                                    }
+                                }
+                            }
+                        }
+                    } perform: {
+                        withAnimation(.easeInOut) {
+                            isSuccess = true
+                        }
+                    }
+                Text("Reset")
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(.black)
+                    .cornerRadius(10)
+                    .onTapGesture {
+                        isComplete = false
+                        isSuccess = false
+                    }
+            }
+            
         }
-        .padding()
     }
 }
+
+//MARK: arguments: minimumDuration, maximumDistance (extra room for user),
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
