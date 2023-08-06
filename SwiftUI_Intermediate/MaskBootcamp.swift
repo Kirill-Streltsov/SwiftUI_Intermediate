990455c6f85a036c24db1fbee3cfd74429a93375
Kirill-Streltsov <krlsrlsv@gmail.com>
2023-08-07 01:52:24 +0200

    mask effect: professional rating view with stars

---
+++ SwiftUI_Intermediate/MaskBootcamp.swift
@@ -0,0 +1,52 @@
+//
+//  MaskBootcamp.swift
+//  SwiftUI_Intermediate
+//
+//  Created by Kirill Streltsov on 07.08.23.
+//
+
+import SwiftUI
+
+struct MaskBootcamp: View {
+    
+    @State var rating = 3
+    
+    var body: some View {
+        ZStack {
+            starsView
+                .overlay(overlayView.mask(starsView))
+        }
+    }
+    
+    private var overlayView: some View  {
+        GeometryReader { geometry in
+            ZStack(alignment: .leading) {
+                Rectangle()
+                    .foregroundColor(.yellow)
+                    .frame(width: CGFloat(rating) / 5 * geometry.size.width)
+            }
+        }
+        .allowsHitTesting(false)
+    }
+    
+    private var starsView: some View {
+        HStack {
+            ForEach(1..<6) {index in
+                Image(systemName: "star.fill")
+                    .font(.largeTitle)
+                    .foregroundColor(.gray)
+                    .onTapGesture {
+                        withAnimation(.easeInOut) {
+                            rating = index
+                        }
+                    }
+            }
+        }
+    }
+}
+
+struct MaskBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        MaskBootcamp()
+    }
+}
