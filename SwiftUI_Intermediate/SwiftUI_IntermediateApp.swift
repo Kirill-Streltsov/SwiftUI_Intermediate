f32f1c62cd4e8a47345e39bc5681307d0b04b3bb
Kirill-Streltsov <krlsrlsv@gmail.com>
2023-08-06 23:34:23 +0200

    Initial Commit

---
+++ SwiftUI_Intermediate/SwiftUI_IntermediateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_IntermediateApp.swift
+//  SwiftUI_Intermediate
+//
+//  Created by Kirill Streltsov on 06.08.23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_IntermediateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
