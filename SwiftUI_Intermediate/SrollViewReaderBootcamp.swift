3ec8c55e3d68063ace42e6b2797d3210defa9d02
Kirill-Streltsov <krlsrlsv@gmail.com>
2023-08-07 00:54:52 +0200

    ScrollViewReader (how whatsApp navigates to new messages)

--- SwiftUI_Intermediate/SrollViewReaderBootcamp.swift
+++ SwiftUI_Intermediate/SrollViewReaderBootcamp.swift
@@ -8,19 +8,51 @@
 import SwiftUI
 
 struct SrollViewReaderBootcamp: View {
+    
+    @State var scrollToIndex: Int = 0
+    @State var textFieldText = ""
+    
     var body: some View {
-        ScrollView {
-            ForEach(0..<50) { index in
-                Text("This is item #\(index)")
-                    .font(.headline)
-                    .frame(height: 200)
-                    .frame(maxWidth: .infinity)
-                    .background(.white)
-                    .cornerRadius(10)
-                    .shadow(radius: 10)
-                    .padding()
+        VStack {
+            TextField("Enter a # here...", text: $textFieldText)
+                .frame(height: 55)
+                .border(.gray)
+                .padding(.horizontal)
+                .keyboardType(.numberPad)
+            
+            Button("SCROLL NOW") {
+                
+                if let index = Int(textFieldText) {
+                    scrollToIndex = index
+                }
+                
+            }
+            
+            ScrollView {
+                ScrollViewReader { proxy in
+                    
+                    
+                    ForEach(0..<50) { index in
+                        Text("This is item #\(index)")
+                            .font(.headline)
+                            .frame(height: 200)
+                            .frame(maxWidth: .infinity)
+                            .background(.white)
+                            .cornerRadius(10)
+                            .shadow(radius: 10)
+                            .padding()
+                            .id(index)
+                        
+                    }
+                    .onChange(of: scrollToIndex) { index in
+                        withAnimation(.spring()) {
+                            proxy.scrollTo(index, anchor: nil)
+                        }
+                    }
+                }
             }
         }
+        
     }
 }
 
