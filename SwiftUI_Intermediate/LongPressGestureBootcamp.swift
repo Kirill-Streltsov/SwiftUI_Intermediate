e41e048d2039c27cf6f42cb8145f61387e518055
Kirill-Streltsov <krlsrlsv@gmail.com>
2023-08-07 00:00:43 +0200

    Instagram picture recreation with MagnificationGesture

--- SwiftUI_Intermediate/LongPressGestureBootcamp.swift
+++ SwiftUI_Intermediate/LongPressGestureBootcamp.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct LongPressGestureBootcamp: View {
     
     @State var isComplete: Bool = false
     @State var isSuccess: Bool = false
@@ -65,6 +65,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        LongPressGestureBootcamp()
     }
 }
---
+++ SwiftUI_Intermediate/MagnificationGestureBootcamp.swift
@@ -0,0 +1,59 @@
+//
+//  MagnificationGestureBootcamp.swift
+//  SwiftUI_Intermediate
+//
+//  Created by Kirill Streltsov on 06.08.23.
+//
+
+import SwiftUI
+
+struct MagnificationGestureBootcamp: View {
+    
+    @State var currentAmount: CGFloat = 0
+    @State var lastAmount: CGFloat = 0
+    
+    var body: some View {
+        
+        VStack {
+            HStack {
+                Circle()
+                    .frame(width: 35, height: 35)
+                Text("Swiftful Thinking")
+                Spacer()
+                Image(systemName: "ellipsis")
+            }
+            .padding(.horizontal)
+            Rectangle()
+                .frame(height: 300)
+                .scaleEffect(1 + currentAmount)
+                .gesture(
+                    MagnificationGesture()
+                        .onChanged { value in
+                            currentAmount = value - 1
+                        }
+                        .onEnded { value in
+                            withAnimation(.spring()) {
+                                currentAmount = 0
+                            }
+                            
+                        }
+                )
+            HStack {
+                Image(systemName: "heart.fill")
+                Image(systemName: "text.bubble.fill")
+                Spacer()
+            }
+            .padding(.horizontal)
+            .font(.headline)
+            Text("This is the caption for my photo!")
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.horizontal)
+        }
+    }
+}
+
+struct MagnificationGestureBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        MagnificationGestureBootcamp()
+    }
+}
