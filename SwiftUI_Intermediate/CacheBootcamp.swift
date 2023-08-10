e57bd8cf15b9c25b8746bd67bfb65728a0d8902e
Kirill-Streltsov <krlsrlsv@gmail.com>
2023-08-10 12:43:12 +0200

    NSCache practice

---
+++ SwiftUI_Intermediate/CacheBootcamp.swift
@@ -0,0 +1,140 @@
+//
+//  CacheBootcamps.swift
+//  SwiftUI_Intermediate
+//
+//  Created by Kirill Streltsov on 10.08.23.
+//
+
+import SwiftUI
+
+class CacheManager {
+    
+    static let instance = CacheManager()
+    private init() {}
+    
+    var imageCache: NSCache<NSString, UIImage> = {
+        let cache = NSCache<NSString, UIImage>()
+        cache.countLimit = 100
+        cache.totalCostLimit = 1024 * 1024 * 100 // 100mb
+        return cache
+    }()
+    
+    func add(image: UIImage, name:  String) {
+        imageCache.setObject(image, forKey: NSString(string: name))
+    }
+    
+    func remove(name: String) {
+        imageCache.removeObject(forKey: NSString(string: name))
+    }
+    
+    func get(name: String) -> UIImage? {
+        return imageCache.object(forKey: NSString(string: name))
+    }
+}
+
+
+class CacheViewModel: ObservableObject {
+    
+    @Published var startingImage: UIImage? = nil
+    @Published var cachedImage: UIImage? = nil
+    let imageName = "steve-jobs"
+    let manager = CacheManager.instance
+    
+    init() {
+        getImageFromAssetsFolder()
+    }
+    
+    func getImageFromAssetsFolder() {
+        startingImage = UIImage(named: imageName)
+    }
+    
+    func saveToCache() {
+        guard let image = startingImage else { return }
+        manager.add(image: image, name: imageName)
+    }
+    
+    func removeFromCache() {
+        manager.remove(name: imageName)
+    }
+    
+    func getFromCache() {
+        cachedImage = manager.get(name: imageName)
+    }
+}
+
+struct CacheBootcamp: View {
+    
+    @StateObject var vm = CacheViewModel()
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                
+                if let image = vm.startingImage {
+                    Image(uiImage: image)
+                        .resizable()
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 200, height: 200)
+                        .clipped()
+                        .cornerRadius(10)
+                    
+                }
+                
+                if let image = vm.startingImage {
+                    Image(uiImage: image)
+                        .resizable()
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 200, height: 200)
+                        .clipped()
+                        .cornerRadius(10)
+                    
+                }
+                
+                HStack {
+                    Button {
+                        vm.saveToCache()
+                    } label: {
+                        Text("Save to Cache")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                            .padding()
+                            .background(.blue)
+                            .cornerRadius(10)
+                    }
+                    Button {
+                        vm.removeFromCache()
+                    } label: {
+                        Text("Delete from Cache")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                            .padding()
+                            .background(.red)
+                            .cornerRadius(10)
+                    }
+                }
+                
+                Button {
+                    vm.getFromCache()
+                } label: {
+                    Text("Get from Cache")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(.green)
+                        .cornerRadius(10)
+                }
+                
+                Spacer()
+            }
+            .navigationTitle("Cache Bootcamp")
+        }
+    }
+}
+
+struct CacheBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        CacheBootcamp()
+    }
+}
